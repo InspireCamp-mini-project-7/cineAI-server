05164c58fbce0998e6d9ea4e69d4f50566e9497b
Subin Cho <109792191+chsubinn@users.noreply.github.com>
2025-02-19 16:07:30 +0900

    chore: Spotless Code Formatter와 Spring Security 설정  (#6)
    * chore: Spotless Code Formatter 적용
    
    * chore: Spring Security 설정

--- src/main/java/com/amcamp/seven/SevenApplication.java
+++ src/main/java/com/amcamp/seven/SevenApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SevenApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SevenApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SevenApplication.class, args);
+    }
 }
