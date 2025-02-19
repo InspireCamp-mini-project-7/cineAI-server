c24eb8831ea3c8740c24671cb8b18ab43329d338
Subin Cho <chosubinn@ewhain.net>
2025-02-19 19:39:24 +0900

    application-test에 redis 설정, test환경에서 profile 활성화

--- src/test/java/com/amcamp/cineAI/CineAIApplicationTests.java
+++ src/test/java/com/amcamp/cineAI/CineAIApplicationTests.java
@@ -2,8 +2,10 @@ package com.amcamp.cineAI;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CineAIApplicationTests {
 
     @Test
