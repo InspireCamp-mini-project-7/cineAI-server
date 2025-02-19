9d1c1adac156637789fa4cd08c24e5972073dd5d
Subin Cho <chosubinn@ewhain.net>
2025-02-19 15:40:59 +0900

    chore: Spotless Code Formatter 적용

--- src/main/java/com/amcamp/seven/infra/config/SwaggerConfig.java
+++ src/main/java/com/amcamp/seven/infra/config/SwaggerConfig.java
@@ -10,12 +10,11 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
-        Info info = new Info()
-                .version("v0.0.1")
-                .title("Seven API") //이름
-                .description("사용자 취향 맞춤형 영화 추천 프로젝트 API"); //설명
-        return new OpenAPI()
-                .info(info);
+        Info info =
+                new Info()
+                        .version("v0.0.1")
+                        .title("Seven API") // 이름
+                        .description("사용자 취향 맞춤형 영화 추천 프로젝트 API"); // 설명
+        return new OpenAPI().info(info);
     }
-
 }
