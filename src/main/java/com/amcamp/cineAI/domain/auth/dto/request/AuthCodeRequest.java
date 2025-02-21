9fea70f9193de245c979bf91f6888aef74a7cf51
Subin Cho <chosubinn@ewhain.net>
2025-02-21 15:54:46 +0900

    refactor: swagger description 추가

--- src/main/java/com/amcamp/cineAI/domain/auth/dto/request/AuthCodeRequest.java
+++ src/main/java/com/amcamp/cineAI/domain/auth/dto/request/AuthCodeRequest.java
@@ -4,5 +4,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 
 public record AuthCodeRequest(
-        @NotBlank(message = "인증 코드는 필수 항목입니다") @Schema(description = "카카오 로그인을 위한 인증 코드")
+        @NotBlank(message = "인증 코드는 필수 항목입니다")
+                @Schema(description = "카카오 로그인을 위한 인증 코드, redirect-url의 code=부터 끝까지입니다.")
                 String code) {}
