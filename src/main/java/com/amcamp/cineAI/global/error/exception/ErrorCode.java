2e3964aa13b655d040474804adcd1b195734f656
Subin Cho <chosubinn@ewhain.net>
2025-02-21 11:03:00 +0900

    fix: auth 예외 처리

--- src/main/java/com/amcamp/cineAI/global/error/exception/ErrorCode.java
+++ src/main/java/com/amcamp/cineAI/global/error/exception/ErrorCode.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorCode {
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "회원을 찾을 수 없습니다."),
-    MEMBER_ALREADY_DELETED(HttpStatus.BAD_REQUEST, "이미 탈퇴한 회원입니다.");
+    MEMBER_ALREADY_DELETED(HttpStatus.BAD_REQUEST, "이미 탈퇴한 회원입니다."),
+    AUTH_NOT_FOUND(HttpStatus.UNAUTHORIZED, "사용자 인증 정보를 찾을 수 없습니다. 올바른 토큰으로 요청해주세요.");
 
     private final HttpStatus status;
     private final String message;
--- src/main/java/com/amcamp/cineAI/global/util/MemberUtil.java
+++ src/main/java/com/amcamp/cineAI/global/util/MemberUtil.java
@@ -5,7 +5,6 @@ import com.amcamp.cineAI.domain.member.domain.Member;
 import com.amcamp.cineAI.domain.member.domain.MemberStatus;
 import com.amcamp.cineAI.global.error.exception.CustomException;
 import com.amcamp.cineAI.global.error.exception.ErrorCode;
-import jakarta.persistence.Column;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -16,22 +15,25 @@ import org.springframework.stereotype.Component;
 public class MemberUtil {
     private final MemberRepository memberRepository;
 
-    public Member getCurrentMember () {
-        Member member = memberRepository.findById(getCurrentMemberId()).orElseThrow(() -> new CustomException(ErrorCode.MEMBER_NOT_FOUND)) ;
+    public Member getCurrentMember() {
+        Member member =
+                memberRepository
+                        .findById(getCurrentMemberId())
+                        .orElseThrow(() -> new CustomException(ErrorCode.MEMBER_NOT_FOUND));
 
         if (member.getStatus() == MemberStatus.DELETED) {
-            throw new CustomException (ErrorCode.MEMBER_ALREADY_DELETED);
+            throw new CustomException(ErrorCode.MEMBER_ALREADY_DELETED);
         }
         return member;
     }
+
     private Long getCurrentMemberId() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
         try {
             return Long.parseLong(authentication.getName());
         } catch (Exception e) {
-            throw ;
+            throw new CustomException(ErrorCode.AUTH_NOT_FOUND);
         }
     }
-
 }
