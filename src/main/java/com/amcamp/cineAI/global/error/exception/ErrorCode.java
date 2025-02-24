b4bf1c1749c330679a45c923dd84744d6c2d757e
오썬 <tyo1012@naver.com>
2025-02-24 22:14:28 +0900

    fix:CI test fail 수정

--- src/main/java/com/amcamp/cineAI/global/error/exception/ErrorCode.java
+++ src/main/java/com/amcamp/cineAI/global/error/exception/ErrorCode.java
@@ -13,7 +13,7 @@ public enum ErrorCode {
     MOVIE_NOT_FOUND(HttpStatus.NOT_FOUND, "영화를 찾을 수 없습니다."),
     CSV_NOT_UPLOAD(HttpStatus.INTERNAL_SERVER_ERROR, "업로드에 실패했습니다."),
     POSTER_NOT_FOUND(HttpStatus.NOT_FOUND, "포스터를 찾을 수 없습니다."),
-    FILE_UPLOAD_FAIL(HttpStatus.INTERNAL_SERVER_ERROR, "파일 저장에 실패했습니다.")
+    FILE_UPLOAD_FAIL(HttpStatus.INTERNAL_SERVER_ERROR, "파일 저장에 실패했습니다."),
     ADMIN_NOT_FOUND(HttpStatus.NOT_FOUND, "관리자 비밀번호가 일치하지 않습니다.");
 
     private final HttpStatus status;
